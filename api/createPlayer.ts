87b5bade13875d1553d1c9fb4b9ef12fed3452ca
Jonathan Van Buren <vanbujm@gmail.com>
2021-08-23 11:17:08 +0800

    Getting auth up 4

--- api/createPlayer.ts
+++ api/createPlayer.ts
@@ -19,9 +19,11 @@ const jwtCheck = jwt({
 });
 
 const createPlayer = async (req: VercelRequest, res: VercelResponse) => {
-  const results = await new Promise((resolve) => jwtCheck(req as any, res as any, resolve));
-  console.log(results);
-  res.status(200).send(JSON.stringify(results));
+  await new Promise((resolve) => jwtCheck(req as any, res as any, resolve));
+  console.log(req.user);
+  console.log('--------------------------');
+  console.log(req);
+  res.status(200).json({});
 };
 
 export default cors(createPlayer);
