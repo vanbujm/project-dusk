daa142fcc072b7a8a1ae0bdefd412b173d3db4cd
Jonathan Van Buren <vanbujm@gmail.com>
2021-08-23 09:28:23 +0800

    Trying auth 5

--- src/Login/LoginButton.tsx
+++ src/Login/LoginButton.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, user, getAccessTokenSilently } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  const createPlayer = useCallback(async () => {
+    const token = await getAccessTokenSilently();
+    const response = await fetch('https://project-dusk.vercel.app/api/createPlayer', {
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        Authorization: token,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  }, [getAccessTokenSilently]);
+
+  if (isAuthenticated) {
+    createPlayer();
+    return null;
+  }
+
+  return <button onClick={loginWithRedirect}>Log In</button>;
 };
