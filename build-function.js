0df96dc01392d2c8b4ea09a480f8db8c5ff77c22
Jonathan Van Buren <vanbujm@gmail.com>
2021-08-28 19:12:32 +0800

    build api 6

--- build-function.js
+++ build-function.js
@@ -1,4 +1,18 @@
 /* eslint-disable */
 console.log('I am being called for functions');
 const { exec } = require('child_process');
-exec('yarn build');
+
+const logCallback = (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: \n${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: \n${stderr}`);
+    return;
+  }
+  console.log(`stdout: \n${stdout}`);
+};
+
+exec('ls', logCallback);
+exec('yarn build', logCallback);
