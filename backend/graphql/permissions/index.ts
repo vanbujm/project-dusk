c7f20b645f578546550f3f925ac9cc8b7b8eca97
Jonathan Van Buren <vanbujm@gmail.com>
2021-08-29 13:33:21 +0800

    fixing graph shield 5

--- backend/graphql/permissions/index.ts
+++ backend/graphql/permissions/index.ts
@@ -1,32 +1,32 @@
 import { and, inputRule, rule, shield } from 'graphql-shield';
 import { Context } from '../resolvers';
 
-const isAuthenticated = rule({ cache: 'contextual' })(async (parent, args, ctx: Context) => {
-  console.log('isAuthenticated', !!ctx?.user?.email);
-  return !!ctx?.user?.email;
-});
+const isAuthenticated = rule({ cache: 'contextual' })(async (parent, args, ctx: Context) => !!ctx?.user?.email);
 
-const isMissing = (str?: string) => {
-  console.log('isMissing', str, !str || str === '');
-  return !str || str === '';
-};
+const isMissing = (str?: string) => !str || str === '';
 
 const hasClassNameOrId = inputRule()(
-  (yup) =>
-    yup.object({
-      where: yup.object({
-        name: yup.string().when('id', {
-          is: isMissing,
-          then: yup.string().required(),
-          otherwise: yup.string(),
-        }),
-        id: yup.string().when('name', {
-          is: isMissing,
-          then: yup.string().required(),
-          otherwise: yup.string(),
-        }),
-      }),
-    }),
+  (yup, ...args) => {
+    console.error(args);
+    return yup
+      .object({
+        where: yup
+          .object({
+            name: yup.string().when('id', {
+              is: isMissing,
+              then: yup.string().required('name required if id not provided'),
+              otherwise: yup.string(),
+            }),
+            id: yup.string().when('name', {
+              is: isMissing,
+              then: yup.string().required('id required if name not provided'),
+              otherwise: yup.string(),
+            }),
+          })
+          .required('"where" arg required'),
+      })
+      .required('narrations query requires an argument');
+  },
   {
     abortEarly: false,
   }
